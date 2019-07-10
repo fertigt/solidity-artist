07958e7ee928565a24d9123e99892202cad20985
Tobias Fertig <tobias.fertig@fhws.de>
2019-07-10 18:40:19 +0200

    added support for mappings as typename

---
+++ src/main/java/com/tobiasfertig/java/solidityartist/MappingTypeName.java
@@ -0,0 +1,29 @@
+package com.tobiasfertig.java.solidityartist;
+
+public class MappingTypeName extends TypeName
+{
+	public final static String MAPPING_KEYWORD = "mapping";
+
+	private TypeName keyType;
+	private TypeName valueType;
+
+	public MappingTypeName( TypeName keyType, TypeName valueType )
+	{
+		super( MAPPING_KEYWORD );
+		this.keyType = keyType;
+		this.valueType = valueType;
+	}
+
+	@Override public void write( CodeWriter writer )
+	{
+		super.write( writer );
+		writer.space( )
+			  .openBraces( )
+			  .write( keyType )
+			  .space( )
+			  .write( "=>" )
+			  .space( )
+			  .write( valueType )
+			  .closeBraces( );
+	}
+}
