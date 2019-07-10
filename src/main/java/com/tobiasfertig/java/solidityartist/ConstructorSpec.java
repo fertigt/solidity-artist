e19e7d1be9eae9508e475ffa7574bdd79dbd6e94
Tobias Fertig <tobias.fertig@fhws.de>
2019-07-10 19:20:00 +0200

    added support for code blocks in constructors

--- src/main/java/com/tobiasfertig/java/solidityartist/ConstructorSpec.java
+++ src/main/java/com/tobiasfertig/java/solidityartist/ConstructorSpec.java
@@ -5,10 +5,12 @@ public class ConstructorSpec implements Writable
 	public final static String CONSTRUCTOR_KEYWORD = "constructor";
 
 	private VisibilityName visibility;
+	private CodeBlock code;
 
 	private ConstructorSpec( Builder builder )
 	{
 		this.visibility = builder.visibility;
+		this.code = (builder.code != null) ? builder.code : CodeBlock.emptyBlock( );
 	}
 
 	public void write( CodeWriter writer )
@@ -17,8 +19,17 @@ public class ConstructorSpec implements Writable
 			  .emptyBraces( )
 			  .space( )
 			  .write( this.visibility )
-			  .space( )
-			  .emptyCurlyBraces( );
+			  .space( );
+
+		if ( code.isEmpty( ) )
+		{
+			writer.emptyCurlyBraces( );
+		} else
+		{
+			writer.openCurlyBraces( )
+				  .write( code )
+				  .closeCurlyBraces( );
+		}
 	}
 
 	public static Builder builder( VisibilityName visibility )
@@ -29,10 +40,17 @@ public class ConstructorSpec implements Writable
 	public static final class Builder
 	{
 		private final VisibilityName visibility;
+		private CodeBlock code;
 
 		private Builder( VisibilityName visibility )
 		{
 			this.visibility = visibility;
+		}
+
+		public Builder addCode( CodeBlock code )
+		{
+			this.code = code;
+			return this;
 		}
 
 		public ConstructorSpec build( )
